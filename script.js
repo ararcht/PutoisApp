75b0d1bbe23e4e49e0fecb5282d86f7fea6f87e6
André Amorim <amorim.andre27@gmail.com>
2019-03-21 18:09:43 +0100

    [BO] added timer reset on good answer and quizz content added

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 $(document).ready(function () {
-    var vies=1;
-    if(window.location.href.indexOf("questions.html") > -1) {
-        timer()
+    var vies = 1;
+    var quest = 1;
+    var list = null;
+    var timer = null;
+    if (window.location.href.indexOf("questions.html") > -1) {
+        timerquest()
     }
-    function timer() {
+
+    function timerquest() {
         var sec = 5;
-        var timer = setInterval(function(){
-            document.getElementById('timer').innerHTML=sec;
+        timer = setInterval(function () {
+            document.getElementById('timer').innerHTML = sec;
             sec--;
             if (sec < 0) {
                 clearInterval(timer);
-                window.location.href="index.html"
+                window.location.href = "index.html"
             }
         }, 1000);
     }
-    $.getJSON( "data.json", function( data ) {
-        console.log(data["Q1"][0]);
+
+    $.ajax({
+        url: 'data.json',
+        async: false,
+        success: function (data) {
+            list = data;
+            $('.question-block').empty();
+            $('.a-answer').empty();
+            $('.b-answer').empty();
+            $('.c-answer').empty();
+            $('.d-answer').empty();
+            $('.question-block').append(data["Q" + quest][0].title);
+            $('.a-answer').append(data["Q" + quest][0].choices[0]);
+            $('.b-answer').append(data["Q" + quest][0].choices[1]);
+            $('.c-answer').append(data["Q" + quest][0].choices[2]);
+            $('.d-answer').append(data["Q" + quest][0].choices[3]);
+        }
+    });
+
+    function newquest() {
+        $('.question-block').empty();
+        $('.a-answer').empty();
+        $('.b-answer').empty();
+        $('.c-answer').empty();
+        $('.d-answer').empty();
+        $('.question-block').append(list["Q" + quest][0].title);
+        $('.a-answer').append(list["Q" + quest][0].choices[0]);
+        $('.b-answer').append(list["Q" + quest][0].choices[1]);
+        $('.c-answer').append(list["Q" + quest][0].choices[2]);
+        $('.d-answer').append(list["Q" + quest][0].choices[3]);
+    }
+
+    $('.answers').click(function () {
+        if ($(this).text() == list['Q' + quest][0].correctAnswer) {
+            quest = quest + 1;
+            newquest();
+            clearInterval(timer);
+            timerquest();
+        } else {
+            window.location.href = "index.html"
+        }
     })
 });
